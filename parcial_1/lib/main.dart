3d39de86c60031ffcc0bc7efe86563842167d535
MiguelS-C <99561755+MiguelS-C@users.noreply.github.com>
2022-02-19 22:09:55 -0600

    Update main.dart

--- parcial_1/lib/main.dart
+++ parcial_1/lib/main.dart
@@ -11,6 +11,7 @@ class Parcial1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Parcial",
       home: Inicio(),
     );
